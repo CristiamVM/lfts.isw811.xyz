a8f91879e8b66665baccdf31f43445570ad4cbd3
CristiamVM <cristian61478@gmail.com>
2023-10-17 00:14:45 -0600

    Ep. 33

--- DOCUMENTACION/Integrate the design/Readme.md
+++ DOCUMENTACION/Integrate the design/Readme.md
@@ -20,14 +20,25 @@
 
 ![Visual Studio Code](./images/post-featured%2032.PNG "Modificacion de vista")
 
-![Visual Studio Code](./images/post-card%2032.PNG.PNG "Modificacion de vista")
+![Visual Studio Code](./images/post-card%2032.PNG "Modificacion de vista")
 
 ![Visual Studio Code](./images/post-grid%2032.PNG "Creacion de vista")
 
 
 
+## 33. Convert the Blog Post Page
+    Ahora le damos forma a nuestro post de la misma manera en la que la hemos venido haciendo
 
-## 33. 
+![Visual Studio Code](./images/post%2033.PNG "Modificacion de vista")
+
+![Visual Studio Code](./images/post-card%2033.PNG "Modificacion de vista")
+
+![Visual Studio Code](./images/post-featured%2033.PNG "Modificacion de vista")
+
+![Visual Studio Code](./images/category-button%2033.PNG "Creacion de vista")
+
+
+
 ## 34. 
 ## 35. 
 ## 36. 
--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,7 +1,6 @@
 @props(['post'])
 
-<article 
-{{$attributes->merge(['class' => 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl'])}}>
+<article {{$attributes->merge(['class' => 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl'])}}>
     <div class="py-6 px-5">
         <div>
             <img src="./images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
@@ -10,7 +9,7 @@
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <a href="/categories\{{$post->category->slug}}" class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold" style="font-size: 10px">{{$post->category->name}}</a>
+                    <x-category-button :category="$post->category" />
                 </div>
 
                 <div class="mt-4">
@@ -26,7 +25,7 @@
 
             <div class="text-sm mt-4">
                 <p>
-                {{ $post-> excerpt }}
+                    {{ $post-> excerpt }}
                 </p>
             </div>
 
@@ -35,7 +34,6 @@
                     <img src="./images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">{{ $post->author->name }}</h5>
-                        <h6>Mascot at Laracasts</h6>
                     </div>
                 </div>
 
